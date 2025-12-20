8e984655f463a10cd687c975c83daa1084d0fa34
yerayafonso <yafonso02@gmail.com>
2025-12-20 20:41:39 +0000

    updated matrix rotation function and added a test file

--- Matrix rotation/matrix-rotation.js
+++ Matrix rotation/matrix-rotation.js
@@ -16,22 +16,7 @@ function rotateMatrix(matrix) {
       newMatrix[i].unshift(matrix[j][i]);
     }
   }
-  //   console.log(newMatrix);
   return newMatrix;
 }
 
-console.log(
-  rotateMatrix([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ])
-);
-console.log(
-  rotateMatrix([
-    [5, 1, 9, 11],
-    [2, 4, 8, 10],
-    [13, 3, 6, 7],
-    [15, 14, 12, 16],
-  ])
-);
+module.exports = rotateMatrix;
